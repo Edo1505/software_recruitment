c5cc399a880a12f0a7ae7ba74874ab98d3585f37
Edo1505 <edoardo.altana@gmail.com>
2026-03-10 18:31:04 +0100

    Implement TemperatureLogger node to log temperatures

--- ROS2/ros2_ws/src/reseq/reseq/temperature_logger.py
+++ ROS2/ros2_ws/src/reseq/reseq/temperature_logger.py
@@ -3,7 +3,27 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 
 # Define the TemperatureLogger node
+class TemperatureLogger(Node):
+    def __init__(self, log_file):
 
+        self.filename = log_file
+
+        super().__init__('temperature_logger')
+        self.subscription = self.create_subscription(
+            Float32,
+            '/temperature',
+            self.temperature_callback,
+            10)
+        
+
+    def temperature_callback(self, msg):
+        temperature = msg.data
+        if temperature > 50.0:
+            log = f"temperature detected: {temperature:.2f}°C"
+            self.get_logger().info(log)
+
+            with open(self.filename, "a") as f:
+                    f.write(f"{log}\n")
 
 def main(args=None):
     rclpy.init(args=args)
